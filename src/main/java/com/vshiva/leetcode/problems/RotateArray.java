a11a685ca7f4819931bc674b1a30ae54dfdb0e69
V. Shivaramakrishnan <shivaramakrishnan.viswanathan@gigamon.com>
2021-09-08 11:48:16 +0530

    Rotate Array

---
+++ src/main/java/com/vshiva/leetcode/problems/RotateArray.java
@@ -0,0 +1,69 @@
+package com.vshiva.leetcode.problems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author sviswanathan
+ * Sep 08, 2021
+ */
+public class RotateArray
+{
+    public static void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        k = n - k;
+        reverse(nums, 0, k-1);
+        //System.out.println(Arrays.toString(nums));
+        reverse(nums, k, n-1);
+        //System.out.println(Arrays.toString(nums));
+        reverse(nums, 0, n-1);
+        //System.out.println(Arrays.toString(nums));
+    }
+
+    public static void reverse(int[] nums, int start, int end) {
+        int tmp = 0;
+        while (start < end) {
+            tmp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = tmp;
+            start++;
+            end--;
+        }
+    }
+
+    public static int[] arrayCopyMethod(int[] nums, int k) {
+        int[] numsCopy = new int[nums.length];
+        int index = 0;
+        for (int i = k + 1; i<nums.length; i++) {
+            numsCopy[index] = nums[i];
+            index++;
+        }
+
+        for (int i = 0; i<=k; i++) {
+            numsCopy[index] = nums[i];
+            index++;
+        }
+
+        System.out.println(Arrays.toString(numsCopy));
+
+        return numsCopy;
+    }
+
+    public static void main(String[] args) {
+        Map<Integer, int[]> valueToListMap = new HashMap<>();
+        valueToListMap.put(3, new int[] { 1, 2, 3, 4, 5, 6, 7 });
+        valueToListMap.put(2, new int[] { -1, -100, 3, 99 });
+        valueToListMap.put(5, new int[] { 1, 2 });
+        valueToListMap.put(4, new int[] { 5 });
+
+        for (var rotateValue : valueToListMap.keySet()) {
+            int[] nums = valueToListMap.get(rotateValue);
+            rotate(nums, rotateValue);
+            System.out.println(Arrays.toString(nums));
+        }
+    }
+}
